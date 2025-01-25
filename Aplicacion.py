7b824ddf9abd040c289e99df37202f0b596b8f66
xDarkBlazer <edwtorressa@unal.edu.co>
2025-01-25 11:51:56 -0500

    Update Aplicacion.py

--- Aplicacion.py
+++ Aplicacion.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def main ():
-  st.title("Clasificación de la base de datos mnist")
+  st.title("Clasificación de la base de datos mnist.")
   st.markdown("Sube una imagen para clasificar")
 
 if __name__=='__main__':
