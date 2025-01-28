d29c29b83f108687187df317373819601292b6eb
xDarkBlazer <edwtorressa@unal.edu.co>
2025-01-28 18:53:38 -0500

    Update Aplicacion.py

--- Aplicacion.py
+++ Aplicacion.py
@@ -4,6 +4,7 @@ from tensorflow.keras.preprocessing.image import img_to_array
 import numpy as np
 import gzip
 import pickle
+import sklearn
 
 def preprocess_image(image):
   image = image.convert('L') #escala grises
