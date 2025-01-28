d726bd41e2d61e4fdc1fd4a095dbfbd61a4a5a28
xDarkBlazer <edwtorressa@unal.edu.co>
2025-01-28 18:37:45 -0500

    Update Aplicacion.py

--- Aplicacion.py
+++ Aplicacion.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from PIL import Image
 from tensorflow.keras.preprocessing.image import img_to_array
+import numpy as np
 
 def preprocess_image(image):
   image = image.convert('L') #escala grises
