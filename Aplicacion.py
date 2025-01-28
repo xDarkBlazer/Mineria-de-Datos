27cf71b44e9295c9340f3fb5d56029ff526dd35d
xDarkBlazer <edwtorressa@unal.edu.co>
2025-01-28 18:23:03 -0500

    Update Aplicacion.py

--- Aplicacion.py
+++ Aplicacion.py
@@ -3,7 +3,7 @@ import streamlit as st
 def main ():
   st.title("Clasificación de la base de datos mnist.")
   st.markdown("Sube una imagen para clasificar")
-  uploaded_file = st.file_uploaded("Seleccione una imagen (PNG, JPG, JPEG:)", type = ["png","jpg","jpeg"])
+  uploaded_file = st.file_uploader("Seleccione una imagen (PNG, JPG, JPEG:)", type = ["png","jpg","jpeg"])
 
 if __name__=='__main__':
   main()
