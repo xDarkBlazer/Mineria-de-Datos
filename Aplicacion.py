36e78eaaeedd7defb17ca209b340067b05ebe918
xDarkBlazer <edwtorressa@unal.edu.co>
2025-01-28 18:31:46 -0500

    Update Aplicacion.py

--- Aplicacion.py
+++ Aplicacion.py
@@ -1,5 +1,14 @@
 import streamlit as st
 from PIL import Image
+from tensorflow.keras.preprocessing.image import img_to_array
+
+def preprocess_image(image):
+  image = image.convert('L') #escala grises
+  image = image.resize((28,28))
+  image_array = img_to_array(image)/255.0
+  image_array = np.expand_dims(image_array, axis=0)
+  return image_array
+  
 
 def main():
   st.title("Clasificación de la base de datos MNIST")
@@ -9,8 +18,9 @@ def main():
 
   if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption = "imagen subida")
-  
+    preprocessed_image = preprocess_image(image)
+    st.image(preprocessed_image, caption = "imagen subida")
+
 
 if __name__ == "__main__":
   main()
