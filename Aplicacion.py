4b136bf73195ba0198a796cf9d6a4fc437b4d915
xDarkBlazer <edwtorressa@unal.edu.co>
2025-01-28 19:07:29 -0500

    Update Aplicacion.py

--- Aplicacion.py
+++ Aplicacion.py
@@ -7,13 +7,13 @@ import pickle
 import sklearn
 
 def preprocess_image(image):
-  image = image.convert('L') #escala grises
+  image = image.convert('L') # convertir a escala de grises
   image = image.resize((28,28))
-  image_array = img_to_array(image)/255.0
+  image_array = img_to_array(image) / 255.0
   image_array = np.expand_dims(image_array, axis=0)
   return image_array
-  
-def load_model ():
+
+def load_model():
   filename = "modelo_entrenado.pkl.gz"
   with gzip.open(filename, 'rb') as f:
     model = pickle.load(f)
@@ -27,13 +27,17 @@ def main():
 
   if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    preprocessed_image = preprocess_image(image) #(1,28,28)
+    st.image(image, caption = "imagen subida")
+
+    preprocessed_image = preprocess_image(image) # (1,28,28)
+   
     st.image(preprocessed_image, caption = "imagen subida")
+
     if st.button("Clasificar imagen"):
       st.markdown("Imagen clasificada")
       model = load_model()
-      
-      prediction = model.predict(preprocessed_image.reshape(1,-1)) #(1, 784)
+
+      prediction = model.predict(preprocessed_image.reshape(1,-1)) # (1, 784)
       st.markdown(f"La imagen fue clasificada como: {prediction}")
 
 
