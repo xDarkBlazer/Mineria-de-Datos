a6a7901dcdc34c728a9db1b922d531ac3057d06a
xDarkBlazer <edwtorressa@unal.edu.co>
2025-02-01 08:51:36 -0500

    Aplicacion.py

--- Aplicacion.py
+++ Aplicacion.py
@@ -14,7 +14,7 @@ def preprocess_image(image):
   return image_array
 
 def load_model():
-  filename = "model_trained.pkl.gz"
+  filename = "model_trained_classifier.pkl.gz"
   with gzip.open(filename, 'rb') as f:
     model = pickle.load(f)
   return model
